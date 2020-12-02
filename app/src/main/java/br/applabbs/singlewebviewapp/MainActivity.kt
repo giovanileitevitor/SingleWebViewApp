c053c9ed557bdee42ccf31eb1e15110d15b6bcf3
Giovani Vitor <giovanileitevitor@gmail.com>
2020-12-02 16:23:17 -0300

    [Projeto - WebView + Kotlin] - versao com suporte a JavaScript

--- app/src/main/java/br/applabbs/singlewebviewapp/MainActivity.kt
+++ app/src/main/java/br/applabbs/singlewebviewapp/MainActivity.kt
@@ -5,9 +5,11 @@ import android.annotation.TargetApi
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +20,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webView.loadUrl("https://www.google.com")
+        //webView.loadUrl("https://www.google.com")
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = object : WebViewClient(){
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                view.loadUrl(url)
-                return false
-            }
+        webView.addJavascriptInterface(this, "testing")
+        webView.loadUrl("file:///android_asset/webview.html")
+//        webView.webViewClient = object : WebViewClient(){
+//            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+//                view.loadUrl(url)
+//                return false
+//            }
+//
+//            @TargetApi(Build.VERSION_CODES.N)
+//            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+//                view.loadUrl(request.url.toString())
+//                return false
+//            }
+//        }
+    }
 
-            @TargetApi(Build.VERSION_CODES.N)
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                view.loadUrl(request.url.toString())
-                return false
-            }
-        }
+    @JavascriptInterface
+    fun showToast(s: String, t: String, u: String){
+        Toast.makeText(this, "Name: $s | Age: $t | BankName: $u", Toast.LENGTH_SHORT).show()
     }
 
     override fun onBackPressed() {
