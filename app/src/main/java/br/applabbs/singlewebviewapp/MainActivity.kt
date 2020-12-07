e3af6398b8f12851671bc1b22ba40ceb88b29ad2
Giovani leite <giovanileitevitor@gmail.com>
2020-12-07 09:51:23 -0300

    Adicionado tela de Loading

--- app/src/main/java/br/applabbs/singlewebviewapp/MainActivity.kt
+++ app/src/main/java/br/applabbs/singlewebviewapp/MainActivity.kt
@@ -2,36 +2,46 @@ package br.applabbs.singlewebviewapp
 
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.app.Activity
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : Activity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webView.loadUrl("https://www.google.com")
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object : WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 view.loadUrl(url)
+                loading_box.visibility = VISIBLE
                 return false
             }
 
             @TargetApi(Build.VERSION_CODES.N)
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
                 view.loadUrl(request.url.toString())
+                loading_box.visibility = VISIBLE
                 return false
             }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                loading_box.visibility = INVISIBLE
+            }
+
         }
+        webView.loadUrl(BuildConfig.ENVIRONMENT_BASE_URL)
     }
 
     override fun onBackPressed() {
